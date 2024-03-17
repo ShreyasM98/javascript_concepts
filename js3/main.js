253342ad476c7998b676e55337e084f9b4307616
Shreyas1092 <shreyas201998@gmail.com>
2024-03-17 08:12:03 +0530

    string methods

--- js3/main.js
+++ js3/main.js
@@ -1,4 +1,23 @@
 const myVariable = "Mathematics";
 
-//the lenghth property
+//the length property
 console.log(myVariable.length);
+
+//string methods
+console.log(myVariable.charAt(5));
+
+console.log(myVariable.indexOf("atic"));
+
+console.log(myVariable.slice(5, 7));
+
+console.log(myVariable.toUpperCase());
+
+console.log(myVariable.toLowerCase());
+
+console.log(myVariable.includes("div"));
+
+console.log(myVariable.split(""));
+
+console.log("John,Joe,Dave".split(","));
+
+console.log("Every good boy does fine".split(" "));
